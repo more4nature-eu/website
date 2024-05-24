f6d61e077dcc93702fa4c33a8512e1010253563a
Andrés González <andres.gonzalez@vizzuality.com>
2024-05-24 09:18:17 +0200

    eslint-plugin-import

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,8 +10,92 @@ module.exports = {
     'import/order': [
       'warn',
       {
-        groups: ['builtin', 'external', 'parent', 'sibling', 'index', 'internal', 'object', 'type'],
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling'],
         'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'builtin',
+            position: 'before',
+          },
+          {
+            pattern: 'react**',
+            group: 'builtin',
+          },
+          {
+            pattern: '@react**',
+            group: 'builtin',
+          },
+          {
+            pattern: 'next/**',
+            group: 'builtin',
+            position: 'after',
+          },
+          {
+            pattern: '@/env.mjs',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/lib/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/data/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/store',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/store/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/types/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/app/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/hooks/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/constants/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/containers/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/components/**',
+            group: 'internal',
+          },
+          {
+            pattern: '@/services/**',
+            group: 'internal',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
       },
     ],
     'no-console': ['warn'],
