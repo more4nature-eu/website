d3a70929cedf192bb323a7e4c79b5cab32540d1c
alexeh <alexurola@gmail.com>
2024-08-06 16:07:29 +0200

    modify event data for easier testing

--- src/data/events.ts
+++ src/data/events.ts
@@ -1,50 +1,50 @@
 const events = [
   {
+    name: 'different',
+    description:
+      '<p>Event description lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p><p>Event description lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>',
+    date: '2025-12-31',
+    image: '/images/events-back/event-1.webp',
+    location: 'thailand',
+  },
+  {
     name: 'vent name lorem ipsum dolor sit amet consectetur.',
     description:
       '<p>Event description lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p><p>Event description lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>',
-    date: '022-12-31',
-    image: '/images/events/event-1.webp',
+    date: '2024-12-31',
+    image: '/images/events-back/event-1.webp',
+    location: 'thailand',
+  },
+  {
+    name: 'vent name lorem ipsum dolor sit amet consectetur.',
+    description:
+      '<p>Event description lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p><p>Event description lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>',
+    date: '2023-12-31',
+    image: '/images/events-back/event-1.webp',
     location: 'street name 5, Copenhagen',
   },
   {
     name: 'vent name lorem ipsum dolor sit amet consectetur.',
     description:
       '<p>Event description lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p><p>Event description lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>',
-    date: '022-12-31',
-    image: '/images/events/event-1.webp',
+    date: '2022-12-31',
+    image: '/images/events-back/event-1.webp',
     location: 'street name 5, Copenhagen',
   },
   {
     name: 'vent name lorem ipsum dolor sit amet consectetur.',
     description:
       '<p>Event description lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p><p>Event description lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>',
-    date: '022-12-31',
-    image: '/images/events/event-1.webp',
+    date: '2022-12-31',
+    image: '/images/events-back/event-1.webp',
     location: 'street name 5, Copenhagen',
   },
   {
     name: 'vent name lorem ipsum dolor sit amet consectetur.',
     description:
       '<p>Event description lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p><p>Event description lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>',
-    date: '022-12-31',
-    image: '/images/events/event-1.webp',
-    location: 'street name 5, Copenhagen',
-  },
-  {
-    name: 'vent name lorem ipsum dolor sit amet consectetur.',
-    description:
-      '<p>Event description lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p><p>Event description lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>',
-    date: '022-12-31',
-    image: '/images/events/event-1.webp',
-    location: 'street name 5, Copenhagen',
-  },
-  {
-    name: 'vent name lorem ipsum dolor sit amet consectetur.',
-    description:
-      '<p>Event description lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p><p>Event description lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>',
-    date: '022-12-31',
-    image: '/images/events/event-1.webp',
+    date: '2022-12-31',
+    image: '/images/events-back/event-1.webp',
     location: 'street name 5, Copenhagen',
   },
 ];
