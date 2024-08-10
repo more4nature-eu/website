6d245f012a40ae87a19e60a27a99e9d06936465d
alexeh <alexurola@gmail.com>
2024-08-10 08:54:14 +0200

    get data and paint case studies

--- src/containers/cases/index.tsx
+++ src/containers/cases/index.tsx
@@ -1,10 +1,19 @@
+import CaseStudyList from '@/containers/cases/list';
+
 import Wrapper from '@/components/ui/wrapper';
+// TODO: Is there a better/native way to achieve this?
+//       First I want this component to take the full height and width of the parent
+
+// TODO: I also want this component to have a transparent background, but because the white is
+// already present for the sidebar, which component should be responsible for the styles here?
+//    Fucking hell this is hard
 
 export default function CaseStudies() {
   return (
-    <div className="bg-grey-800">
-      <Wrapper className="space-y-16">
+    <div className="h-full w-full">
+      <Wrapper className="h-full w-full space-y-16 bg-transparent">
         <h4 className="text-xl text-white">CaseStudies</h4>
+        <CaseStudyList />
       </Wrapper>
     </div>
   );
---
+++ src/containers/cases/list/index.tsx
@@ -0,0 +1,114 @@
+'use client';
+
+import { useState } from 'react';
+
+import { useQuery, keepPreviousData } from '@tanstack/react-query';
+
+import { CaseStudy } from '@/lib/case-studies.service';
+import { News } from '@/lib/news.service';
+import { PaginatedResult } from '@/lib/paginator';
+import queryKeys from '@/lib/query-keys';
+
+import DiscoverMoreButton from '@/components/discover-more-button';
+import { Paginator } from '@/components/paginator';
+import { Badge } from '@/components/ui/badge';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import formatDate from '@/utils/date';
+
+// function NewsItem({ name, date, description, categories }: News) {
+//   return (
+//     <div className="grid grid-cols-12 border-t border-t-white/30 py-16 text-white">
+//       <span className="col-span-3 uppercase">{formatDate(date)}</span>
+//       <div className="col-span-6 space-y-4">
+//         <h5 className="text-xl">{name}</h5>
+//         {categories?.length > 0 && (
+//           <div className="flex gap-4">
+//             {categories.map((category) => (
+//               <span key={category} className="text-sm">
+//                 <Badge className="pointer-events-none">{category}</Badge>
+//               </span>
+//             ))}
+//           </div>
+//         )}
+//       </div>
+//       <div className="col-span-3 flex items-start justify-end">
+//         <Dialog>
+//           <DialogTrigger asChild>
+//             <DiscoverMoreButton />
+//           </DialogTrigger>
+//           <DialogContent>
+//             <DialogHeader>
+//               <DialogTitle>{name}</DialogTitle>
+//               <DialogDescription>
+//                 <div
+//                   className="space-y-4"
+//                   dangerouslySetInnerHTML={{
+//                     __html: description,
+//                   }}
+//                 />
+//               </DialogDescription>
+//             </DialogHeader>
+//           </DialogContent>
+//         </Dialog>
+//       </div>
+//     </div>
+//   );
+// }
+
+const PAGE_SIZE = 5;
+
+export default function CaseStudyList() {
+  const [page, setPage] = useState(0);
+
+  const { data } = useQuery<PaginatedResult<CaseStudy>>({
+    queryKey: queryKeys.cases.paginated({ page: page + 1 }).queryKey,
+    queryFn: async () => {
+      try {
+        const response = await fetch(`/case-studies?page=${page + 1}&pageSize=${PAGE_SIZE}`);
+
+        if (!response.ok) {
+          throw new Error('Error fetching cases');
+        }
+        return await response.json();
+      } catch (err) {
+        throw new Error('Error fetching cases');
+      }
+    },
+    placeholderData: keepPreviousData,
+  });
+
+  return (
+    <>
+      <ul>
+        {data?.data?.map((news) => (
+          <li key={news.name} className="text-red-500">
+            {news.name}
+          </li>
+        ))}
+      </ul>
+      {/*<Paginator*/}
+      {/*  pageIndex={page}*/}
+      {/*  pageCount={Math.ceil(*/}
+      {/*    (data?.total as NonNullable<PaginatedResult<News>['total']>) / PAGE_SIZE,*/}
+      {/*  )}*/}
+      {/*  totalPagesToDisplay={6}*/}
+      {/*  onPagePrevious={() => {*/}
+      {/*    setPage(page - 1);*/}
+      {/*  }}*/}
+      {/*  onPageNext={() => {*/}
+      {/*    setPage(page + 1);*/}
+      {/*  }}*/}
+      {/*  onPageIndex={(p) => {*/}
+      {/*    setPage(p);*/}
+      {/*  }}*/}
+      {/*/>*/}
+    </>
+  );
+}
