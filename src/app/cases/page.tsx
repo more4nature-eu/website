05cd3344bd2c662aa2cf9af6f025b6bbe399f544
alexeh <alexurola@gmail.com>
2024-08-10 08:39:18 +0200

    prefetch case studies with queryClient

--- src/app/cases/page.tsx
+++ src/app/cases/page.tsx
@@ -1,8 +1,33 @@
+import { QueryClient } from '@tanstack/react-query';
+
+import { CaseStudyService } from '@/lib/case-studies.service';
+import queryKeys from '@/lib/query-keys';
+
+import CASE_STUDIES from '@/data/case-studies';
+
 import Header from '@/containers/cases/header';
 import Map from '@/containers/cases/map';
 import Sidebar from '@/containers/cases/sidebar';
 
-export default function Cases() {
+const INITIAL_NEWS_PAGE = 1;
+
+const prefetchData = async (queryClient: QueryClient) => {
+  const caseStudiesService = new CaseStudyService(
+    CASE_STUDIES,
+    {},
+    { page: INITIAL_NEWS_PAGE, pageSize: 5 },
+  );
+  const paginatedCaseStudies = caseStudiesService.searchCaseStudies();
+
+  queryClient.setQueryData(
+    queryKeys.cases.paginated({ page: INITIAL_NEWS_PAGE }).queryKey,
+    paginatedCaseStudies,
+  );
+};
+
+export default async function Cases() {
+  const queryClient = new QueryClient();
+  await prefetchData(queryClient);
   return (
     <>
       <Header />
