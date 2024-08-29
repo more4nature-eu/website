c3344fe4937180b101b926ac800344ef1ae003eb
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2024-08-29 19:20:51 +0200

    wip

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -136,25 +136,22 @@ const Carousel = React.forwardRef<
 });
 Carousel.displayName = 'Carousel';
 
-const CarouselContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => {
-    const { carouselRef, orientation } = useCarousel();
+const CarouselContent = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement> & { infinite?: boolean }
+>(({ className, infinite, ...props }, ref) => {
+  const { carouselRef, orientation } = useCarousel();
 
-    return (
-      <div ref={carouselRef} className="overflow-hidden">
-        <div
-          ref={ref}
-          className={cn(
-            'flex',
-            orientation === 'horizontal' ? '-ml-4' : '-mt-4 flex-col',
-            className,
-          )}
-          {...props}
-        />
-      </div>
-    );
-  },
-);
+  return (
+    <div ref={carouselRef} className={infinite ? 'overflow-visible' : 'overflow-hidden'}>
+      <div
+        ref={ref}
+        className={cn('flex', orientation === 'horizontal' ? '-ml-4' : '-mt-4 flex-col', className)}
+        {...props}
+      />
+    </div>
+  );
+});
 CarouselContent.displayName = 'CarouselContent';
 
 const CarouselItem = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
