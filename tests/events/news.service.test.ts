ac3e1e05e8c1d9c1c92cdf223fc5c3f9763a0b10
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2024-10-08 18:01:07 +0200

    updates news tests

--- tests/events/news.service.test.ts
+++ tests/events/news.service.test.ts
@@ -49,17 +49,17 @@ describe('NewsService', () => {
         name: 'European Space Agency Environmental Crimes Workshop 2024',
         date: '2024-06-11',
         description:
-          "<p>We're excited to launch the more4nature project, focusing on zero pollution, biodiversity protection, and deforestation prevention by empowering citizens and communities. The project aims to bridge the gap between Citizen Science initiatives (CSIs) and local authorities, enhancing environmental compliance and monitoring. By strengthening CSIs, facilitating collaboration, and developing tools to validate Citizen-Generated Data (CGD), more4nature seeks to make CGD a key part of the Green Deal Data Space, driving green and digital transformations.</p>",
+          '<p>At the JRC and ESA Environmental Crimes Workshop 2024, Joan Maso Pau from CREAF presented a poster on "Empowering Citizens in Collaborative Environmental Compliance Assurance." Joan highlighted how more4nature drove conservation by involving citizens and communities in efforts to achieve zero pollution, protect biodiversity, and prevent deforestation. The presentation covered strengthening Citizen Science Initiatives for reliable data, enhancing collaboration between Citizen Science initiatives and authorities, developing tools for validating and integrating citizen data, incorporating data into the Green Deal Data Space, and fostering synergies with LivingLabs and Fab Labs. We engaged with experts and explored how remote sensing could complement citizen science.</p>',
         categories: ['Deforestation Prevention', 'Biodiversity Protection', 'Zero Pollution'],
-        link: 'https://www.linkedin.com/feed/update/urn:li:activity:7169301090432200704',
+        link: 'https://www.linkedin.com/feed/update/urn:li:activity:7206272288357298176',
       },
       {
-        name: 'Leveraging Citizen Science Initiatives',
-        date: '2024-04-25',
+        name: 'more4nature Project in Copenhagen!',
+        date: '2024-05-30',
         description:
-          "<p>It’s exciting to see the discussion on societal engagement and open science. At more4nature, we're actively contributing to this dialogue by empowering citizens and communities in environmental action. Our project is all about leveraging Citizen Science initiatives (CSIs) to tackle pollution, protect biodiversity, and prevent deforestation.</p><p>Citizen involvement and citizen-generated data (CGD) are vital to counter environmental degradation. That's why more4nature is dedicated to bridging the gap between CSIs and authorities. By strengthening CSIs, facilitating collaboration, and developing tools for validating CGD, we ensure that valuable data doesn't go to waste.</p>",
+          '<p>Our team is thrilled to reunite in Copenhagen, advancing transformative conservation efforts by integrating citizens and Sitizen Science initiatives into environmental compliance. Discussions with European Environment Agency (EEA) experts highlighted shared interests in using Citizen-Generated Data for monitoring at national and regional levels.</p> <p>We explored links between health, pollution, and the need for better data on issues like microplastics. More4nature partner Ida Theilade emphasized, "Citizens can play a role because they know what to look for," despite challenges like illegal logging.</p>',
         categories: ['Deforestation Prevention', 'Biodiversity Protection', 'Zero Pollution'],
-        link: 'https://www.linkedin.com/feed/update/urn:li:activity:7189277848879124481',
+        link: 'https://www.linkedin.com/feed/update/urn:li:activity:7201890677780041728',
       },
       {
         name: 'Earth Day Workshop with Coastwatch',
@@ -70,18 +70,10 @@ describe('NewsService', () => {
         link: 'https://www.linkedin.com/feed/update/urn:li:activity:7189583716824854528',
       },
       {
-        name: 'more4nature Project in Copenhagen!',
-        date: '2024-05-30',
+        name: 'Leveraging Citizen Science Initiatives',
+        date: '2024-04-25',
         description:
-          '<p>Our team is thrilled to reunite in Copenhagen, advancing transformative conservation efforts by integrating citizens and Citizen Science initiatives into environmental compliance. Discussions with European Environment Agency (EEA) experts highlighted shared interests in using Citizen-Generated Data for monitoring at national and regional levels.</p> <p>We explored links between health, pollution, and the need for better data on issues like microplastics. More4nature partner Ida Theilade emphasized, "Citizens can play a role because they know what to look for," despite challenges like illegal logging.</p>',
-        categories: ['Deforestation Prevention', 'Biodiversity Protection', 'Zero Pollution'],
-        link: 'https://www.linkedin.com/feed/update/urn:li:activity:7201890677780041728',
-      },
-      {
-        name: 'Earth Day Workshop with Coastwatch',
-        date: '2024-04-26',
-        description:
-          '<p>At the JRC and ESA Environmental Crimes Workshop 2024, Joan Maso Pau from CREAF presented a poster on "Empowering Citizens in Collaborative Environmental Compliance Assurance." Joan highlighted how more4nature drove conservation by involving citizens and communities in efforts to achieve zero pollution, protect biodiversity, and prevent deforestation. The presentation covered strengthening Citizen Science Initiatives for reliable data, enhancing collaboration between Citizen Science initiatives and authorities, developing tools for validating and integrating citizen data, incorporating data into the Green Deal Data Space, and fostering synergies with LivingLabs and Fab Labs. We engaged with experts and explored how remote sensing could complement citizen science.</p>',
+          "<p>It’s exciting to see the discussion on societal engagement and open science. At more4nature, we're actively contributing to this dialogue by empowering citizens and communities in environmental action. Our project is all about leveraging Citizen Science initiatives (CSIs) to tackle pollution, protect biodiversity, and prevent deforestation.</p><p>Citizen involvement and citizen-generated data (CGD) are vital to counter environmental degradation. That's why more4nature is dedicated to bridging the gap between CSIs and authorities. By strengthening CSIs, facilitating collaboration, and developing tools for validating CGD, we ensure that valuable data doesn't go to waste.</p>",
         categories: ['Deforestation Prevention', 'Biodiversity Protection', 'Zero Pollution'],
         link: 'https://www.linkedin.com/feed/update/urn:li:activity:7189277848879124481',
       },
@@ -89,7 +81,7 @@ describe('NewsService', () => {
         name: 'Introduction to more4nature Project',
         date: '2024-01-03',
         description:
-          "<p>We're excited to launch the more4nature project, focusing on zero pollution, biodiversity protection, and deforestation prevention by empowering citizens and communities. The project aims to bridge the gap between citizen science initiatives (CSIs) and local authorities, enhancing environmental compliance and monitoring. By strengthening CSIs, facilitating collaboration, and developing tools to validate Citizen-Generated Data (CGD), more4nature seeks to make CGD a key part of the Green Deal Data Space, driving green and digital transformations.</p>",
+          "<p>We're excited to launch the more4nature project, focusing on zero pollution, biodiversity protection, and deforestation prevention by empowering citizens and communities. The project aims to bridge the gap between Citizen Science initiatives (CSIs) and local authorities, enhancing environmental compliance and monitoring. By strengthening CSIs, facilitating collaboration, and developing tools to validate Citizen-Generated Data (CGD), more4nature seeks to make CGD a key part of the Green Deal Data Space, driving green and digital transformations.</p>",
         categories: ['Deforestation Prevention', 'Biodiversity Protection', 'Zero Pollution'],
         link: 'https://www.linkedin.com/feed/update/urn:li:activity:7169301090432200704',
       },
@@ -108,7 +100,7 @@ describe('NewsService', () => {
         date: '2024-07-03',
         description:
           '<p>On June 25, representatives from Hutsulshchyna National Nature Park, part of the more4nature project by the National Ecological Centre of Ukraine, visited the Carpathian National Nature Park in Yaremche. The event aimed to raise awareness about forest management reforms and illegal logging, engaging 75 local citizens in discussions. Later, the delegation also visited Vorokhta and the Vorokhta Forest Management Branch, presenting the more4nature project and surveying 30 forestry workers. The day was highly productive and informative for all involved.</p>',
-        categories: [ThematicArea.DEFORESTATION_PREVENTION],
+        categories: ['Deforestation Prevention'],
         link: 'https://www.linkedin.com/feed/update/urn:li:activity:7214292712999546880',
       },
       {
@@ -116,11 +108,11 @@ describe('NewsService', () => {
         date: '2024-06-27',
         description:
           "<p>Exciting news on the EU's Nature Restoration Law! Portuguese Minister Maria Da Graça Carvalho recently emphasized the importance of protecting pollinators, highlighting the Action Plan led by Universidade de Coimbra and featured in more4nature's case studies. This plan aims to combat pollinator decline through improved conservation efforts, data collection via citizen science, and increased public awareness. PolinizAÇÃO, part of polli.NET and coordinated by the FLOWer Lab, is a key initiative in this effort. Kudos to more4nature partners Cristina Luís and Esther Marín González for their work on this crucial Biodiversity Protection case.</p>",
-        categories: [ThematicArea.BIODIVERSITY_PROTECTION],
+        categories: ['Biodiversity Protection'],
         link: 'https://www.linkedin.com/feed/update/urn:li:activity:7212008106069766144',
       },
     ]);
-    expect(result.total).toBe(10);
+    expect(result.total).toBe(11);
     expect(result.page).toBe(1);
     expect(result.pageSize).toBe(2);
   });
@@ -136,9 +128,9 @@ describe('NewsService', () => {
         name: 'Hutsulshchyna Park Team, Part of more4nature Project, Raises Awareness on Forest Reforms in Carpathian National Park',
         date: '2024-07-03',
         description:
-          "<p>We're excited to launch the more4nature project, focusing on zero pollution, biodiversity protection, and deforestation prevention by empowering citizens and communities. The project aims to bridge the gap between Citizen Science initiatives (CSIs) and local authorities, enhancing environmental compliance and monitoring. By strengthening CSIs, facilitating collaboration, and developing tools to validate Citizen-Generated Data (CGD), more4nature seeks to make CGD a key part of the Green Deal Data Space, driving green and digital transformations.</p>",
-        categories: ['Deforestation Prevention', 'Biodiversity Protection', 'Zero Pollution'],
-        link: 'https://www.linkedin.com/feed/update/urn:li:activity:7169301090432200704',
+          '<p>On June 25, representatives from Hutsulshchyna National Nature Park, part of the more4nature project by the National Ecological Centre of Ukraine, visited the Carpathian National Nature Park in Yaremche. The event aimed to raise awareness about forest management reforms and illegal logging, engaging 75 local citizens in discussions. Later, the delegation also visited Vorokhta and the Vorokhta Forest Management Branch, presenting the more4nature project and surveying 30 forestry workers. The day was highly productive and informative for all involved.</p>',
+        categories: ['Deforestation Prevention'],
+        link: 'https://www.linkedin.com/feed/update/urn:li:activity:7214292712999546880',
       },
     ]);
     expect(result.total).toBe(5);
